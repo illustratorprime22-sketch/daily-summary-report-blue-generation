f2ac51199ded52e2ce920a370623c0ad0b0c953b
Mayur Kambli <mayur.kambli@pdc.com>
2026-08-11 18:47:41 -0400

    Update email recipients

--- blue_generation_report.py
+++ blue_generation_report.py
@@ -16,8 +16,8 @@ SMTP_PORT = 465
 SENDER_EMAIL = 'mayur.kambli@artworkservicesusa.com'
 
 # Recipients
-RECIPIENTS_TO = ['mayur.online9@gmail.com']
-RECIPIENTS_CC = []
+RECIPIENTS_TO = ['tricial@bluegeneration.com']
+RECIPIENTS_CC = ['mattr@bluegeneration.com', 'rupesh.pardeshi@artworkservicesusa.com']
 RECIPIENTS_BCC = ['mayur.kambli@artworkservicesusa.com']
 
 def get_google_client():
